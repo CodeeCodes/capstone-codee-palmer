a6a9d9a603e3b83bba559a414acdd8b5dbb3def7
Codee Palmer <codeejpalmer@gmail.com>
2019-11-26 00:39:42 -0800

    Last commit for day, working on geolocatng position from google developers api

--- client/src/components/fitMap.js
+++ client/src/components/fitMap.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import GoogleMapsStyles from "../styles/partials/googleMapsStyles.json";
+import runningIcon from "../assets/iconRunning.png";
 export default function FitMap() {
   const stylesArray = GoogleMapsStyles;
-  const [cords, setCords] = useState({
-    lat: 49.2827,
-    lng: 123.1207
-  });
+  // const [cords, setCords] = useState({
+  //   lat: 49.2827,
+  //   lng: 123.1207
+  // });
 
   useEffect(() => {
     renderMap();
   });
+  // const apiKey = "AIzaSyB0ebv97Ph2LJRAvh9fO6Sd3F7gVxGp1ss";
+  // const googleFindUrl = `https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}`;
+  // const findRunner = axios.get(googleFindUrl).then(res => {
+  //   console.log(res);
+  // });
 
   const renderMap = () => {
     loadScript(
@@ -22,6 +29,12 @@ export default function FitMap() {
       center: { lat: 49.2827, lng: -123.1207 },
       zoom: 13,
       styles: stylesArray
+    });
+    let marker = new window.google.maps.Marker({
+      position: { lat: 49.2827, lng: -123.1207 },
+      map: map,
+      title: "Hello Vancouver",
+      icon: runningIcon
     });
   };
 
