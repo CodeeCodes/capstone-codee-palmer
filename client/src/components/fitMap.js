0c05039b32512344fe39ec0b2c9d71b940662465
Codee Palmer <codeejpalmer@gmail.com>
2019-11-25 23:32:56 -0800

    Linked google maps without using a library

--- client/src/components/fitMap.js
+++ client/src/components/fitMap.js
@@ -1,9 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function FitMap() {
   const [cords, setCords] = useState({
     lat: 49.2827,
     lng: 123.1207
   });
-  return <div className="map"></div>;
+
+  useEffect(() => {
+    renderMap();
+  });
+
+  const renderMap = () => {
+    loadScript(
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyB0ebv97Ph2LJRAvh9fO6Sd3F7gVxGp1ss&callback=initMap"
+    );
+    window.initMap = initMap;
+  };
+  const initMap = () => {
+    const map = new window.google.maps.Map(document.querySelector(".map"), {
+      center: { lat: 49.2827, lng: 123.1207 },
+      zoom: 8
+    });
+  };
+
+  return (
+    <div className="map__main">
+      {" "}
+      <div className="map"></div>
+    </div>
+  );
+  function loadScript(Url) {
+    let index = window.document.getElementsByTagName("script")[0];
+    let script = window.document.createElement("script");
+    script.src = Url;
+    script.async = true;
+    script.defer = true;
+    index.parentNode.insertBefore(script, index);
+  }
 }
